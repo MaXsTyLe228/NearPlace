7c373aea7e5548ebe5118cbd66ce88901fd93d56
Kashlyatskii Maksym <maksym.kashlyatskii@sysgears.com>
2021-08-21 00:49:02 +0300

    Initial commit

--- src/Business/SearchPlace.ts
+++ src/Business/SearchPlace.ts
@@ -3,25 +3,35 @@ import getResult from '../API/GetServResult'
 export default async function searchPlace(): Promise<string> {
     let responseJson: any = JSON.parse(await getResult());
     let venues: any = responseJson.response.venues;
-    console.log(responseJson);//
-    let place: string;
+    console.log(responseJson);
+    let place: string = '';
     if (venues[0] === undefined) {
         place = 'Рядом мест нет(';
     }
     else {
         let distance: number = venues[0].location.distance;
         let position: number;
+        let distarr = new Object();
         for (let i in venues) {
             if (distance >= venues[i].location.distance) {
                 distance = venues[i].location.distance;
                 position = +i;
+                distarr[position] = distance;
             }
         }
-        place = `Ближайшая точка: ${venues[position].name}\n` +
-            `Расстояние: ${venues[position].location.distance}\n`
-        if (venues[position].categories[0] !== undefined) {
-            place += `Тип: ${venues[position].categories[0].name}\n`;
+        console.log(distarr);
+        for (let i in distarr) {
+            console.log(venues[i])
+            if (venues[i].location.distance <= distance) {
+                console.log(distance)
+                place += `Ближайшая точка: ${venues[i].name}\n` +
+                    `Расстояние: ${venues[i].location.distance}\n`;
+                if (venues[i].categories[0] !== undefined) {
+                    place += `Тип: ${venues[i].categories[0].name}\n`;
+                }
+            }
         }
+
     }
-    return place;
+    return place;//44.953435, 45.039815 -- coordinats for 2 places
 }
