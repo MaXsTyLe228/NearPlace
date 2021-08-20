aab2d5f0795c7bfd8f29f48ceb9957e0d3ea4e09
Kashlyatskii Maksym <maksym.kashlyatskii@sysgears.com>
2021-08-20 21:34:38 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,6 @@ import clickEventListener from './Business/ClickEvent';
 
 clickEventListener();
 
-
-function valideteCoordinats(x: number, y: number): number[] {
-    if ((y <= 180 && y >= -180) && (x <= 90 && x >= -90) && (x !== 0 || y !== 0)) {
-        return [x, y];
-    } else {
-        alert('Некорректные данные');
-        return null;
-    }
-}
-console.log(valideteCoordinats(90, 180.00000))
 /*import createConstParamPath from './API/ForesquareAPI'
 import parametersObject from './API/constApi';
 
