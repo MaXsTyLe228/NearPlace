cf0e2eec44b47b0f7225e6a031bd37a50ab70ac4
Kashlyatskii Maksym <maksym.kashlyatskii@sysgears.com>
2021-08-26 00:01:57 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import clickEventListener from './Business/ClickEvent';
 
-clickEventListener();
+class EventObserver {
+
+    observers: Array<any>
+
+    constructor() {
+        this.observers = []
+    }
+
+    subscribe(fn) {
+        this.observers.push(fn)
+    }
+
+    unsubscribe(fn) {
+        this.observers = this.observers.filter(subscriber => subscriber !== fn)
+    }
+
+    broadcast(data) {
+        this.observers.forEach(subscriber => subscriber(data))
+    }
+}
+
+const observer = new EventObserver()
+
+observer.subscribe(clickEventListener());
